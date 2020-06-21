48ef6cb0a266327a03b66e954c9ae6a8f504dd6f
Nathaniel <nathaniel@scriptingis.life>
2020-06-21 10:33:08 -0400

    make lambda packages

---
+++ lambda/pack.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DIST_DIR="../dist"
+LAMBDA_BUCKET="s3eker-lambda-store"
+
+mkdir dist 2>/dev/null
+
+for D in */
+do
+    DIR_NAME="${D%%/}" # Remove slash at end of dir name. dir/ to dir
+    if [ "$DIR_NAME" == "dist" ]
+    then
+        continue
+    fi
+
+    cd $D
+    echo "Packing $D"
+    ZIP_FILENAME="$DIR_NAME.zip"
+    ZIP_FILE="$DIST_DIR/$ZIP_FILENAME"
+    zip "$ZIP_FILE"  main.py
+    aws s3 cp "$ZIP_FILE" "s3://$LAMBDA_BUCKET/$DIR_NAME/$ZIP_FILENAME"
+done
