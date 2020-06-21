c18dbd34386ac698b78133ced2a3e48d3055e3c9
Nathaniel <nathaniel@scriptingis.life>
2020-06-21 17:51:57 -0400

    remove interpolation

--- infra/lambda-scan.tf
+++ infra/lambda-scan.tf
@@ -63,16 +63,16 @@ resource "aws_iam_role_policy_attachment" "attach-scan-webhook-read" {
 resource "aws_lambda_permission" "allow_bucket" {
   statement_id  = "AllowExecutionFromS3Bucket"
   action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.scan.arn}"
+  function_name = aws_lambda_function.scan.arn
   principal     = "s3.amazonaws.com"
-  source_arn    = "${aws_s3_bucket.buckets.arn}"
+  source_arn    = aws_s3_bucket.buckets.arn
 }
 
 resource "aws_s3_bucket_notification" "bucket_notification" {
-  bucket = "${aws_s3_bucket.buckets.id}"
+  bucket = aws_s3_bucket.buckets.id
 
   lambda_function {
-    lambda_function_arn = "${aws_lambda_function.scan.arn}"
+    lambda_function_arn = aws_lambda_function.scan.arn
     events              = ["s3:ObjectCreated:*"]
   }
 
