937965c69c39d513358138f9552788636e8ab18e
Nathaniel <nathaniel@scriptingis.life>
2020-06-21 11:30:51 -0400

    remove tabs from policy

--- infra/s3-buckets.tf
+++ infra/s3-buckets.tf
@@ -7,21 +7,20 @@ resource "aws_iam_policy" "buckets-write" {
     name = "s3eker-s3-buckets-write"
     description = "Allows writing to S3 bucket used for storing bucket domains."
     policy = <<EOF
+{
+"Version": "2012-10-17",
+"Statement": [
     {
-    "Id": "Policy1592752777775",
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-        "Sid": "Stmt1592752776654",
-        "Action": [
-            "s3:PutObject"
-        ],
-        "Effect": "Allow",
-        "Resource": "${aws_s3_bucket.buckets.arn}/*",
-        "Principal": "*"
-        }
-    ]
+    "Sid": "VisualEditor0",
+    "Action": [
+        "s3:PutObject"
+    ],
+    "Effect": "Allow",
+    "Resource": "${aws_s3_bucket.buckets.arn}/*",
+    "Principal": "*"
     }
+]
+}
     EOF
 }
 
@@ -29,20 +28,19 @@ resource "aws_iam_policy" "buckets-read" {
     name = "s3eker-s3-buckets-write"
     description = "Allows reading S3 bucket used for storing bucket domains."
     policy = <<EOF
+{
+"Version": "2012-10-17",
+"Statement": [
     {
-    "Id": "Policy1592752777774",
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-        "Sid": "Stmt1592752776653",
-        "Action": [
-            "s3:GetObject"
-        ],
-        "Effect": "Allow",
-        "Resource": "${aws_s3_bucket.buckets.arn}/*",
-        "Principal": "*"
-        }
-    ]
+    "Sid": "VisualEditor0",
+    "Action": [
+        "s3:GetObject"
+    ],
+    "Effect": "Allow",
+    "Resource": "${aws_s3_bucket.buckets.arn}/*",
+    "Principal": "*"
     }
+]
+}
     EOF
 }
