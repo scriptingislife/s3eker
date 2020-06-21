c3d7e19c1de3211f7360f657049c4c1104b1d5be
Nathaniel <nathaniel@scriptingis.life>
2020-06-21 14:59:56 -0400

    add dependencies to deployment

--- lambda/pack.sh
+++ lambda/pack.sh
@@ -1,23 +1,41 @@
 #!/bin/bash
 
-DIST_DIR="../dist"
 LAMBDA_BUCKET="s3eker-lambda-store"
+ORIG_DIR="$PWD"
 
 mkdir dist 2>/dev/null
 
 for D in */
 do
-    DIR_NAME="${D%%/}" # Remove slash at end of dir name. dir/ to dir
-    if [ "$DIR_NAME" == "dist" ]
+    FUNCTION_NAME="${D%%/}" # Remove slash at end of dir name. dir/ to dir
+    if [ "$FUNCTION_NAME" == "dist" ]
     then
         continue
     fi
 
-    cd $D
+    TARGET_DIR="./dist/$D"
+
     echo "Packing $D"
-    ZIP_FILENAME="$DIR_NAME.zip"
+    
+    # Install dependencies in dist/FUNCTION_NAME/
+    cp -r "$D" "$TARGET_DIR"
+    if [ -f "$FUNCTION_NAME/requirements.txt" ]; then
+        pip3 install -r "$FUNCTION_NAME/requirements.txt" --target "$TARGET_DIR"
+    fi
+    
+    cd "$TARGET_DIR"
+    
+    DIST_DIR=".."
+    ZIP_FILENAME="$FUNCTION_NAME.zip"
     ZIP_FILE="$DIST_DIR/$ZIP_FILENAME"
-    zip "$ZIP_FILE"  main.py
-    aws s3 cp "$ZIP_FILE" "s3://$LAMBDA_BUCKET/$DIR_NAME/$ZIP_FILENAME"
-    aws lambda update-function-code --function-name "s3eker-$DIR_NAME" --s3-bucket $LAMBDA_BUCKET --s3-key "$DIR_NAME/$ZIP_FILENAME"
+    
+    if zip "$ZIP_FILE"  ./*; then
+        echo "Successfully zipped $FUNCTION_NAME"
+        aws s3 cp "$ZIP_FILE" "s3://$LAMBDA_BUCKET/$FUNCTION_NAME/$ZIP_FILENAME"
+        aws lambda update-function-code --function-name "s3eker-$FUNCTION_NAME" --s3-bucket $LAMBDA_BUCKET --s3-key "$FUNCTION_NAME/$ZIP_FILENAME"
+    
+    fi    
+    
+    cd $ORIG_DIR
+
 done
