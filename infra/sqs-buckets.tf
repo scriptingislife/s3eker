d930a552eada8951be0b50c4f31ce718a5324e2c
Nathaniel <nathaniel@scriptingis.life>
2020-07-22 09:04:14 -0400

    remove sqs infra

--- infra/sqs-buckets.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_sqs_queue" "buckets" {
-    name = "s3eker-buckets"
-    max_message_size            = 2048
-    message_retention_seconds   = 86400
-    receive_wait_time_seconds   = 10
-}
